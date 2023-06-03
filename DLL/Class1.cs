9e18cb331d77c45ae3cdb4467d0f001dee595729
sin <daniilnovosletsev@gmail.com>
2023-06-03 18:21:13 +0300

    Добавьте файлы проекта.

---
+++ DLL/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DLL
+{
+    public class Class1
+    {
+        public int disk(int a, int b, int c)
+        { 
+            
+            return (b*b) - (4*a*c);
+        }
+
+    }
+}
